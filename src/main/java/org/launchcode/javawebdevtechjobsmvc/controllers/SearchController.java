b99c1fa8f74f004bafdddd027b760d82413d8674
Chris Carriere <chris.carriere323@gmail.com>
2019-12-10 19:56:43 -0600

    completed displaySearchResults handeler and search.html template

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,24 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType ) {
+        ArrayList<Job> jobs;
+
+        if (searchTerm.toLowerCase() == "all" || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            if (searchType.equals("all")) {
+                jobs = JobData.findByValue(searchTerm);
+            } else {
+                jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            }
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("searchType", searchType);
+        return "search";
+
+    }
 }
